1e2d9993e06e19fc3ca50dc7b40122b5e8be2de6
Myles Iribarne <myles.iribarne@gmail.com>
2023-11-28 15:26:43 -0300

    add history saved in cookies to site

--- js/translate.js
+++ js/translate.js
@@ -18,7 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // get user prompt from textbox.
         let examples_txt = getExamplesText();
-        let inputText = document.getElementById('inputText').value;
+        let userInputText = document.getElementById('inputText').value;
+        let inputText = userInputText;
         let temperature = 0 // by default, lower temperature to get highest probability output
         if (!inputText) {
             // if text input is empty, prompt chatgpt to output an interesting phrase
@@ -46,6 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(content)
 
         // try to get prompt from open ai
+        let outputMsg = "";
         try {
             let response = await fetch("https://api.openai.com/v1/chat/completions", {
                 method: "POST",
@@ -85,9 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let processedMsg = lines.join('\n');
 
             // conver markdown to html and put into pages
-            let markedMsg = marked.parse(processedMsg)
-            // console.log(markedMsg)
-            document.getElementById('outputText').innerHTML = markedMsg;
+            outputMsg = marked.parse(processedMsg);
         } 
         catch (error) {
             // if response indicates an openai key error, prompt user to update
@@ -104,28 +104,101 @@ document.addEventListener('DOMContentLoaded', function() {
             alert("Failed to fetch translation.");
         }
 
+
+        // Save this data for history
+        if (outputMsg){
+            saveTranslationData(userInputText, selectedCountry, outputMsg);
+            // and now load this into gui
+            currentIndex = 0;
+            loadTranslationData(currentIndex);
+        }
+        
         // reenable button once done with translation
         button.disabled = false; 
         button.innerText = "Translate!"
     }
 
+    ///////////////////////////////////////////
     // set selected country based on saved cookie
+    //////////////////////////////////////////////
     let defaultCountry = Cookies.get('selectedCountry');
     if (defaultCountry){
-        document.getElementById('languageSelect').value = Cookies.get('selectedCountry');
+        document.getElementById('languageSelect').value = defaultCountry;
     }
 
-    // add event handler for button press
+    /////////////////////////////////
+    // add left right button callbacks
+    /////////////////////////////////
+    let currentIndex = -1;
+
+    function saveTranslationData(input, language, output) {
+        let translations = JSON.parse(localStorage.getItem("translations")) || [];
+        translations.unshift({ input, language, output }); // Add new translation at the start
+        localStorage.setItem("translations", JSON.stringify(translations));
+    }
+    
+    // Function to load translation data
+    function loadTranslationData(index) {
+        let translations = JSON.parse(localStorage.getItem("translations")) || [];
+        let maxIndex = translations.length - 1;
+
+        // Update GUI enable/disable status
+        document.getElementById('rightButton').disabled = index <= -1;
+        document.getElementById('leftButton').disabled = index >= maxIndex;
+        
+        if (index < 0){
+            document.getElementById('inputText').value = "";
+            document.getElementById('outputText').innerHTML = "";
+        }
+        else if(index < translations.length) {
+            let data = translations[index];
+            document.getElementById('inputText').value = data.input;
+            document.getElementById('languageSelect').value = data.language;
+            document.getElementById('outputText').innerHTML = data.output;
+        }
+    }
+
+    document.getElementById('leftButton').addEventListener('click', function() {
+        if (currentIndex < JSON.parse(localStorage.getItem("translations")).length - 1) {
+            currentIndex++;
+            loadTranslationData(currentIndex);
+        }
+    });
+
+    document.getElementById('rightButton').addEventListener('click', function() {
+        if (currentIndex > -1) {
+            currentIndex--;
+            loadTranslationData(currentIndex);
+        }
+    });
+
+    ///////////////////////
+    /// add translate button callback
+    //////////////////////
     document.getElementById('translateButton').addEventListener('click', translateText);
 
+    // Initial call to set up the UI
+    loadTranslationData(currentIndex);
+
+    ///////////////////
+    // add keyboard functionality
+    ////////////////////////
+    // add event handler for button press
+
     // make enter press "translate", and shift+enter add a newling
-    document.getElementById('inputText').addEventListener('keydown', function(event) {
+    document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter' && !event.shiftKey) {
             // Prevent default Enter behavior (newline)
             event.preventDefault();
             // Trigger button click
             document.getElementById('translateButton').click();
         }
+        else if (event.key === 'ArrowLeft') {
+            document.getElementById('leftButton').click();
+        } else if (event.key === 'ArrowRight') {
+            document.getElementById('rightButton').click();
+        }
     });
+    
 
 });
